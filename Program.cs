1280d0fdc11317dff5f2682256eb2349f09911f8
PedroHds <112220933+PedroHenriquePDS@users.noreply.github.com>
2022-11-11 21:37:02 -0300

    Adiciona a solução do exercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("----- Tabuada -----");
+
+System.Console.Write("Digite um número que deseja fazer a tabuada: ");
+
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+int multiplo = 1;
+int resultado;
+
+
+while ( multiplo <= 10)
+{
+    resultado = numeroDigitado * multiplo;
+    System.Console.WriteLine($"{numeroDigitado} X {multiplo} = {resultado}");
+    multiplo++;
+}
+
+System.Console.Write("Pressione qualquer tecla para fechar");
+Console.ReadKey();
+
+
